4a0bd0f4c8e38a409a85ffa9e95d7b2682b975a8
Alberto <ajsaenz1091@gmail.com>
2021-07-27 21:55:38 -0400

    Completed Assignment

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,35 @@
+class PlantsController < ApplicationController
+    def  index
+        plants = Plant.all
+        render json: plants
+    end
+
+    def show
+        plant = Plant.find(params[:id])
+        if plant
+            render json: plant
+        else
+            render json: { error: "Plant not found" }, status: :not_found
+        end
+    end
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+
+    # def update
+    #     plant = Plant.find(params[:id])
+    #     if plant
+    #         plant.update(plant_params)
+    #         render json: plant
+    #     else
+    #         render json: { error: "Plant not found" }, status: :not_found
+    #     end
+    # end
+
+end
